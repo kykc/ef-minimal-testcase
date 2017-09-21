464a494ca92ee7f64755cbbfccd922a502358213
Alexander Prokopchuk <a.prokopchuk@braingines.com>
2017-09-22 00:48:52 +0300

    initial commit

---
+++ Migrations/20170921214729_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace efcminimaltestcase.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BusinessTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AmountCharged_Amount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    AmountCharged_Currency = table.Column<int>(type: "int", nullable: false),
+                    Profit_Amount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Profit_Currency = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusinessTransactions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusinessTransactions");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace efc_minimal_testcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+	public enum Currency
+	{
+		EUR = 1,
+		USD = 2,
+	}
+
+	public class MoneyAmount
+	{
+		public MoneyAmount(decimal amount, Currency currency)
+		{
+			Amount = amount;
+			Currency = currency;
+		}
+
+		public Currency Currency { get; private set; }
+		public decimal Amount { get; private set; }
+	}
+
+	public class BusinessTransaction
+	{
+		public int Id { get; set; }
+		public MoneyAmount AmountCharged { get; set; }
+		public MoneyAmount Profit { get; set; }
+	}
+
+	public class FinancialDbContext : DbContext
+	{
+		public DbSet<BusinessTransaction> BusinessTransactions { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//optionsBuilder.UseInMemoryDatabase("FinancialTest");
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=FinancialTestCase;Trusted_Connection=True;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<BusinessTransaction>().OwnsOne(p => p.Profit);
+			modelBuilder.Entity<BusinessTransaction>().OwnsOne(p => p.AmountCharged);
+			//modelBuilder.Entity<BusinessTransaction>().OwnsOne(p => p.ChargeFee);
+		}
+	}
+}
